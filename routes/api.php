9fac43b47915cbd8d1ec00ea2f71f71016d88517
jhraj963 <jhraj963@gmail.com>
2025-03-03 00:15:20 +0600

    Added Role-Based Access Control with Spatie

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+});
